43192e74e9a5744416ca66fda636bb744dd94a03
Ananya Sristy <ansristy@gmail.com>
2025-11-10 12:20:01 -0500

    b37

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 import express from "express";
+import employeesRouter from "#api/employees";
 const app = express();
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Fullstack Employees API.");
+});
+app.use("/employees", employeesRouter);
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Internal server error' });
+})
 export default app;
-
-// TODO: this file!
